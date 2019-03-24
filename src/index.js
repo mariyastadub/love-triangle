442f261aab4bc07eb9c46e28ffef0300cc292744
Mariya Stadub <mariya_lunkina@mail.ru>
2019-03-24 20:37:09 +0300

    task solution

--- src/index.js
+++ src/index.js
@@ -4,4 +4,16 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
-};
+
+  var count = 0;
+  preferences.unshift(0);
+  
+  for(var i = 1; i < preferences.length; i++){
+      var a = preferences[i];
+      var b = preferences[a];
+      
+      count += i == preferences[b];
+  }
+
+  return Math.floor(count/3);
+}
